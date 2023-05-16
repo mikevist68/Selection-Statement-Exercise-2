d27df7dfad2ff871920b7c072d714f9aa5df0eec
Michael Vistocci <mikevist68@yahoo.com>
2023-05-16 14:24:31 -0400

    completed

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -6,5 +6,47 @@
         {
             Console.WriteLine("Hello, World!");
         }
+        
+        public static void FavSubject()
+        {
+
+            Console.WriteLine("Whats your favorite subject?");
+            var subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I uesd to like math");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is okay");
+                    break;
+                case "pe":
+                    Console.WriteLine("I miss pe");
+                    break;
+                case "history":
+                    Console.WriteLine("History was fun sometimes");
+                    break;
+                case "music":
+                    Console.WriteLine("I had alot of fun in music class");
+                    break;
+
+
+            }
+        
+        
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
